4b6d92660f1880920c2d1f0dff92a4de2dd53af7
MOON_CLJ <lijunli2598@gmail.com>
2014-12-19 23:57:31 +0800

    add gen_maestro_yaml.py

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 start:
-	docker run -i -t -p 8080:8080 --net=host weibo_emotion:1.0
+	docker run -i -t -p 8080:8080 --net=host weibo_emotion:1.1
---
+++ gen_maestro_yaml.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import yaml
+
+cluster_name = "weibo-emotion-cluster"
+ships = ["219.224.135.93"]
+base_image = "weibo_emotion:1.1"
+base_port = 9000
+num_instance_per_ship = 10
+
+
+def gen_maestro_yaml():
+    with open("maestro.yaml", "w") as conf:
+        data = {
+            "name": cluster_name,
+            "ships": {},
+            "services": {
+                "weibo-emotion-app": {
+                    "image": base_image,
+                    "instances": {}
+                },
+            },
+            "audit": [
+                {"type": "log", "file": "/tmp/maestro"}
+            ]
+        }
+        for ship in ships:
+            data["ships"][ship] = {"ip": ship}
+
+        for i in xrange(len(ships)):
+            for j in xrange(num_instance_per_ship):
+                container_no = i * num_instance_per_ship + j
+                service_name = "weibo-emotion-app"
+                container_name = "%s%s" % (service_name, container_no)
+                container_port = base_port + container_no
+                data["services"][service_name]["instances"][container_name] = {
+                    "ship": ships[i],
+                    "ports": {"client": {"external": container_port, "exposed": 8080}},
+                }
+
+        yaml.dump(data, conf, default_flow_style=False)
+
+if __name__ == "__main__":
+    gen_maestro_yaml()
