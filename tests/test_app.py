ea11380aa2d63e2d447d9726f2d8b7008571a3fe
MOON_CLJ <lijunli2598@gmail.com>
2014-12-19 23:02:18 +0800

    fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,8 +9,19 @@ class AppTestCase(unittest.TestCase):
         super(AppTestCase, self).setUp()
         self._url = "http://127.0.0.1:8080/diamond_classifier"
 
-    def test_diamond_classifier(self):
+    def test_no_text(self):
+        r = requests.post(self._url)
+        self.assertEqual(r.text, '-1')
+        self.assertEqual(r.status_code, 200)
+
+    def test_text_hehe(self):
         payload = {'text': 'hehe'}
         r = requests.post(self._url, data=payload)
         self.assertEqual(r.text, '1')
+        self.assertEqual(r.status_code, 200)
+
+    def test_text_sad(self):
+        payload = {'text': 'sad'}
+        r = requests.post(self._url, data=payload)
+        self.assertEqual(r.text, '0')
         self.assertEqual(r.status_code, 200)
