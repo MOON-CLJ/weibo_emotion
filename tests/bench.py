1b98d07bf9b227356fef84587f01a3d8fcb57817
MOON_CLJ <lijunli2598@gmail.com>
2014-12-20 23:24:24 +0800

    fix

--- tests/bench.py
+++ tests/bench.py
@@ -4,6 +4,8 @@ import requests
 
 url = 'http://127.0.0.1:8080/diamond_classifier'
 
+s = requests.Session()
+payload = {'text': u'呵呵'}
+
 while 1:
-    payload = {'text': u'呵呵'}
-    requests.post(url, data=payload)
+    s.post(url, data=payload)
