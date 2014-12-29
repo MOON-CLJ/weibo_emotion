004599286b10e44ec5cc13a102d4ac503bd75c51
MOON_CLJ <lijunli2598@gmail.com>
2014-12-29 22:39:19 +0800

    fix

--- tests/bench.py
+++ tests/bench.py
@@ -7,13 +7,13 @@ url_fmt = 'http://127.0.0.1:%s/diamond_classifier'
 
 urls = [url_fmt % (8000 + i) for i in range(10)]
 
-sessions = [requests.Session() for i in range(10)]
+s = requests.Session()
 payload = '呵呵'
 
 count = 1
 last = time.time()
 while 1:
-    sessions[count % 10].post(urls[count % 10], data=payload)
+    s.post(urls[count % 10], data=payload)
     count += 1
     if count % 1000 == 0:
         now = time.time()
